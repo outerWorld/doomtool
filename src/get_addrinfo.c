a0692e892c7a10f64ba60f05128fd52d2b652ba2
chunping <chunping@BJWS110371.(none)>
2012-12-31 17:45:06 +0800

    transfer number ip to string ip

--- src/get_addrinfo.c
+++ src/get_addrinfo.c
@@ -6,17 +6,24 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 static void print_addrinfo(struct addrinfo * ai)
 {
+	char _ip[128] = { 0 };
 	struct sockaddr_in * addr;
 	fprintf(stdout, "flags = %d, family = %d, socktype = %d, protocol = %d\n",
 			ai->ai_flags, ai->ai_family, ai->ai_socktype, ai->ai_protocol);
 	addr = (struct sockaddr_in *)ai->ai_addr;
-	if (ai->ai_addr)
-		fprintf(stdout, "sockaddr_size = %d, sockaddr = [%08x, %d, %d], cononame = %s\n", 
-				ai->ai_addrlen, addr->sin_addr.s_addr, addr->sin_port, addr->sin_family, ai->ai_canonname);
+	if (ai->ai_addr) {
+		if (NULL == inet_ntop(addr->sin_family, (void*)&addr->sin_addr, _ip, 128)) {
+			fprintf(stderr, "error calling inet_ntop [%s]\n", strerror(errno));
+			return;
+		}
+		fprintf(stdout, "sockaddr_size = %d, sockaddr = [%s, %d, %d], cononame = %s\n", 
+				ai->ai_addrlen, _ip,  ntohs(addr->sin_port), addr->sin_family, ai->ai_canonname);
+	}
 }
 
 /*
