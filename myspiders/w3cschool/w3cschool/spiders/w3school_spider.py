4dc65e518cabc6e0d03b7fa9634890349b848ce8
xxx.xxx.xxx.xxx <xxx-xxx-xxx-xxx@xxx-xxx-xxx-xxx.(none)>
2013-01-05 00:24:38 +0800

    add parser for main page and sub page

--- myspiders/w3cschool/w3cschool/spiders/w3school_spider.py
+++ myspiders/w3cschool/w3cschool/spiders/w3school_spider.py
@@ -1,6 +1,7 @@
 # coding: UTF-8
 from scrapy.spider import BaseSpider
 from scrapy.http import Request
+from scrapy.selector import HtmlXPathSelector
 from w3cschool.url_parser import url_parser
 import sys
 import os
@@ -29,9 +30,20 @@ class w3school_spider(BaseSpider):
 			pass
 
 	def main_url_parser(self, url_obj, response):
-		pass
+		print "main_url_parser: %s" %(response.url)
+		# store the page
+		open(self.root_path + "/" + "index.html", "w").write(response.body)
+		# fetch the sub pages from this main page.
+		hxs = HtmlXPathSelector(response)
+		item_list = hxs.select(r'//div[@id="navsecond"]/ul/li/a')	
+		for i in range(0, len(item_list)):
+			#print "item [%s] [%s]" %("".join(item_list[i].select(r'@href').extract()), "".join(item_list[i].select(r'text()').extract()))
+			req_url = self.host + "".join(item_list[i].select(r'@href').extract())
+			print "Request url = %s" %(req_url)
+			yield Request(req_url, 'GET')
 
 	def sub_url_parser(self, url_obj, response):
+		print "sub_url_parser: %s" %(response.url)
 		pass
 
 	def dispatch_parse(self, url_obj, response):
@@ -43,6 +55,7 @@ class w3school_spider(BaseSpider):
 		else:
 			pass
 		
+		print "uri_path = %s" %(uri_path)
 		if uri_path == "/":
 			self.main_url_parser(url_obj, response)
 		else:
