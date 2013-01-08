93918472c1dad935a6e92af75454003af8939ffa
chunping <chunping@BJWS110371.(none)>
2013-01-08 18:01:37 +0800

    add forbid_ip.c and calculator.c

---
+++ src/calculator.c
@@ -0,0 +1,15 @@
+/*
+ * practice for tree structure.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * Usage: calculator math expression
+ *	 ex. calculator (1*2+3)^10
+ */
+int main(int argc, char *argv[])
+{
+	return 0;
+}
