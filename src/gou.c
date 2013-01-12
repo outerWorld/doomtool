a891c86e1cba6a9baae3b2367b7e57d33a20cedc
xxx.xxx.xxx.xxx <xxx-xxx-xxx-xxx@xxx-xxx-xxx-xxx.(none)>
2013-01-13 02:12:19 +0800

    add url parser in gou.c

--- src/gou.c
+++ src/gou.c
@@ -12,6 +12,8 @@
 #include <signal.h>
 
 #include "f1g_basic_funcs.h"
+#include "f1g_http_parser.h"
+#include "f1g_url_parser.h"
 #include "f1g_server.h"
 
 static i8_p http_header_rsp = //"Cache-Control:private, max-age=0, must-revalidate\r\n"
@@ -22,28 +24,61 @@ static i8_p http_header_rsp = //"Cache-Control:private, max-age=0, must-revalida
 							  "Server:GoU 0.0.1 \r\n";
 static i8_p http_content = "<html><head><title>Test Server GoU</title></head><body><h1>GoU, the best choice.</h1></body></html>";
 
+typedef struct _http_proc_context_s {
+	url_parser_t		url_parser;
+	http_req_header_t 	req_header;
+}http_proc_context_t, *http_proc_context_p;
+
 /*
  *
  */
 static i32_t http_proc(context_t ctx, buffer_p p_rdbuf, buffer_p p_wrbuf, i32_t *proc_st)
 {
+	http_proc_context_p p_ctx = (http_proc_context_p)ctx;
+	http_req_header_p p_req_header = &p_ctx->req_header;
 
+	*proc_st = PROC_ST_INIT;
+#if 1
 	// Http resolver
-
+	http_req_header_clear(p_req_header);
+	http_req_header_parser(p_req_header, buffer_data(p_rdbuf), buffer_data_len(p_rdbuf));
+	http_req_header_print(p_req_header);
+#endif
 	// url resolver
-
+#if 1
+	url_parser_clear(&p_ctx->url_parser);
+	if (F1G_OK != url_parser_load(&p_ctx->url_parser, p_req_header->uri.data, p_req_header->uri.len)) {
+		*proc_st = PROC_ST_ERR;
+		return F1G_ERR;
+	}
+	url_parser_show(&p_ctx->url_parser);
+#endif
 	// response
 	buffer_append_format(p_wrbuf, "%s", "HTTP/1.1 200 OK\r\n");
 	buffer_append_format(p_wrbuf, "%s", http_header_rsp);
 	buffer_append_format(p_wrbuf, "Conent-Length: %d\r\n", strlen(http_content));
 	buffer_append_format(p_wrbuf, "\r\n%s", http_content);
-	//buffer_show(p_wrbuf);
+	buffer_show(p_wrbuf);
 
 	*proc_st = PROC_ST_SENDBUF;
 
 	return F1G_OK;
 }
 
+
+static i32_t http_proc_context_clean(void *ctx)
+{
+	http_proc_context_p p_ctx = (http_proc_context_p)ctx;
+	if (p_ctx) {
+		url_parser_destroy(&p_ctx->url_parser);
+	}
+
+	return F1G_OK;
+}
+
+void ignore(int signo)
+{
+}
 
 /*
  * Usage: GoU --serv_conf=conf_file -d 
@@ -56,6 +91,7 @@ int main(int argc, char *argv[])
 	i32_t	is_daemon = 0;
 	i8_p 	fpath = NULL;
 	server_conf_t conf;
+	http_proc_context_t ctx;
 	serv_object_p p_serv = NULL;
 
 	while (-1 != (result = getopt(argc, argv, "c:d"))) {
@@ -77,13 +113,14 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	signal(SIGPIPE, SIG_IGN);
 	if (is_daemon) {
 		if (F1G_OK != set_daemon(argv[0])) {
 			fprintf(stderr, "%s set deamon fail \n", argv[0]);
 			return 1;
 		}
 	}
-	signal(SIGPIPE, SIG_IGN);
+	signal(SIGPIPE, ignore);
 	
 	if (F1G_OK != serv_load(fpath, &conf)) {
 		return 1;
@@ -93,7 +130,8 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	if (F1G_OK != serv_set_work_cb(p_serv, NULL, http_proc, NULL, 0, NULL)) {
+	url_parser_init(&ctx.url_parser, 0);
+	if (F1G_OK != serv_set_work_cb(p_serv, NULL, http_proc, &ctx, sizeof(ctx), http_proc_context_clean)) {
 		return 1;
 	}
 
