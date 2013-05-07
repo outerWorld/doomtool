8d2150700eb188ce5728ee2495bfb41c7c4a4003
xxx.xxx.xxx.xxx <xxx-xxx-xxx-xxx@xxx-xxx-xxx-xxx.(none)>
2013-05-08 00:29:02 +0800

    add

--- alg/sort.c
+++ alg/sort.c
@@ -5,11 +5,26 @@
 
 #include "sort.h"
 
+// support 
+//		basic types: int, char *, char**
+//		self defined types
+//
+static int sort_ins(void *data, int size, int elem_size, cmp_func_p p_cmp, swap_func_p p_swap)
+{
+}
 
 int sort(sort_data_p p_data)
 {
 
 	switch (p_data->method) {
+		case SORT_INSERTION:
+			break;
+		case SORT_BUBBLE:
+			break;
+		case SORT_QUICK:
+			break;
+		default:
+			return -1;
 	}
 
 	return 0;
--- alg/sort.h
+++ alg/sort.h
@@ -8,14 +8,22 @@
 #ifndef __SORT_H
 #define __SORT_H
 
+enum {
+	SORT_INSERTION = 0x00, //
+	SORT_BUBBLE,
+	SORT_QUICK,
+};
+
 typedef int (*cmp_func_p)(void *d1, void *d2);
 typedef int	(*swap_func_p)(void *d1, void *d2);
 
 typedef struct _sort_data_s {
-	void 	*unsorted;
-	int		e_size;
-	int		e_num;
-	int		method;
+	void 		*unsorted;
+	int			e_size;
+	int			e_num;
+	int			method;
+	cmp_func_p 	p_cmp;
+	swap_func_p p_swap;
 }sort_data_t, *sort_data_p;
 
 int sort(sort_data_p p_data);
